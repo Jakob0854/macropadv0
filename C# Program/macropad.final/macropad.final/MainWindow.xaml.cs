17ed028d6da8c83731ef65b627bf3faadb1ed0bb
Jakob0854 <111630255+Jakob0854@users.noreply.github.com>
2023-04-16 14:27:53 +0200

    Add files via upload

---
+++ C# Program/macropad.final/macropad.final/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO.Ports;
+using System.Threading;
+
+using System.Diagnostics;
+
+
+
+namespace macropad.final
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        System.IO.Ports.SerialPort serpor = new System.IO.Ports.SerialPort();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://learn.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        private void comcheck_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        
+        private void Aktualisieren_Click(object sender, RoutedEventArgs e)
+        {
+            string pn = comcheck.SelectedValue.ToString();
+            serpor.PortName = pn;
+        }
+
+        private void Layout_Click(object sender, RoutedEventArgs e)
+        {
+            serpor.Open();
+            serpor.WriteLine("displ_layout");
+            serpor.Close();
+        }
+
+        private void Uhrzeit_Click(object sender, RoutedEventArgs e)
+        {
+            serpor.Open();
+            serpor.WriteLine("displ_set time");
+            Thread.Sleep(3);
+            serpor.WriteLine(DateTime.Now.ToString("HH"));
+            Thread.Sleep(3);
+            serpor.WriteLine(DateTime.Now.ToString("mm"));
+            serpor.Close();
+        }
+        private void Off_Click(object sender, RoutedEventArgs e)
+        {
+            serpor.Open();
+            serpor.WriteLine("displ_off");
+            serpor.Close();
+        }
+
+        private void Fade_Click(object sender, RoutedEventArgs e)
+        {
+            serpor.Open();
+            serpor.WriteLine("set rgb_fade");
+            serpor.Close();
+        }
+
+        private void Custom_Click(object sender, RoutedEventArgs e)
+        {
+            string sliderRval = sliderR.Value.ToString();
+            string sliderGval = sliderG.Value.ToString();
+            string sliderBval = sliderB.Value.ToString();
+            serpor.Open();
+            serpor.WriteLine("set rgb_custom color");
+            Thread.Sleep(3);
+            serpor.WriteLine(sliderRval);
+            Thread.Sleep(3);
+            serpor.WriteLine(sliderGval);
+            Thread.Sleep(3);
+            serpor.WriteLine(sliderBval);
+            serpor.Close();
+        }
+
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+        }
+
+        private void sliderG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void sliderB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            serpor.BaudRate = (115200);
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            serpor.Open();
+            serpor.WriteLine("save to littlefs");
+            serpor.Close();
+        }
+    }
+}
